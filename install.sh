d85b628e297a76c2952a22fd6a296873f6ec5ce6
wtfil <filmuzhik@gmail.com>
2014-02-27 12:26:13 +0200

    fixed adding screen to autoload

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 DIR=~/ubuntu-env
+PROFILE=~/.bashrc
 git clone git@github.com:wtfil/ubuntu-env.git $DIR
 if hash apt-get 2>/dev/null; then
     sudo apt-get install screen
     sudo chmod +s /usr/bin/screen
     sudo chmod 755 /var/run/screen
-    cat $DIR/.auto-screen.sh >> ~/.bashrc
-    echo ok;
+    if [ -z `grep '#autoload screen' $PROFILE` ]; then
+        cat '#autoload screen' >> $PROFILE
+        cat $DIR/.auto-screen.sh >> $PROFILE
+    fi;
+    cp $DIR/.screenrc ~
 else
-    echo 'apt-get in not find';
+    echo 'apt-get in not find; screen is not instaled';
 fi;
-cp .vimrc ~
-#git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+cp $DIR/.vimrc ~
+cp -r $DIR/.vim ~
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
