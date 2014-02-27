dba1cd6dd15fa63090d48f7f60e45ab73ebbb271
wtfil <filmuzhik@gmail.com>
2014-02-27 12:16:56 +0200

    fixed path

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 DIR=~/ubuntu-env
-git clone git@github.com:wtfil/ubuntu-env.git $DIR && cd $DIR
+git clone git@github.com:wtfil/ubuntu-env.git $DIR
 if hash apt-get 2>/dev/null; then
     sudo apt-get install screen
     sudo chmod +s /usr/bin/screen
     sudo chmod 755 /var/run/screen
-    cat .auto-screen.sh >> ~/.bashrc
+    cat $DIR/.auto-screen.sh >> ~/.bashrc
     echo ok;
 else
     echo 'apt-get in not find';
 fi;
-#cp .vimrc ~
+cp .vimrc ~
 #git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
