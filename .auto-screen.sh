5633a209b83004fd5add3026e58711252b47ff2d
wtfil <filmuzhik@gmail.com>
2014-03-06 15:29:27 +0200

    added new aliases; removed .auto-screen.sh

--- .auto-screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCREEN_NAME="$USER"vim;
-if [[ $TERM != screen* ]]; then
-    if [ `screen -ls | grep $SCREEN_NAME` -z ]; then
-        screen -S $SCREEN_NAME;
-    else
-        screen -x $USER/$SCREEN_NAME;
-    fi;
-fi;
--- .bash_aliases
+++ .bash_aliases
@@ -1,5 +1,7 @@
 function mcd() {
     mkdir -p "$1" && cd "$1";
 }
+alias gr='grep -snr --color=always'
+alias egr='egrep -snr --color=always'
 alias ll='ls -halF'
 alias ss='du -sh'
