a781fb18ae1f44de241555e926235b76d89be758
wtfil <wtfil@orgsearch.serp.yandex.ru>
2014-06-13 15:43:19 +0400

    fixed vundle git repo

--- install.sh
+++ install.sh
@@ -21,6 +21,6 @@ cp $DIR/.bash_aliases ~
 cp $DIR/.vimrc ~
 cp -r $DIR/.vim ~
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 . ~/.bashrc
