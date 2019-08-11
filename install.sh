5af5ea2b84a0d1ebf0236692a3bc5a47d143a7c3
Evgen Filatov <evgen.filatov@gmail.com>
2019-08-11 10:26:25 +0300

    Install: added installation of node/yarn/typescript/python

--- install.sh
+++ install.sh
@@ -4,17 +4,24 @@ PROFILE=~/.bashrc
 NVIM=~/.config/nvim
 
 if hash apt-get 2>/dev/null; then
-    sudo apt-get install -y screen git vim curl
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update -y
+    sudo apt-get install -y python3 python3-pip vim neovim curl node
 fi;
 if hash yum 2>/dev/null; then
     sudo yum -y install git
     sudo yum -y install vim
+    sudo yum -y install node
     sudo yum -y install screen
 fi;
 
+npm install -g npm yarn typescript n neovim
+n latest
+pip3 install --user pynvim
+
 git clone git@github.com:wtfil/dot-files.git $DIR
 
-if hash screen 2>/dev/null; then 
+if hash screen 2>/dev/null; then
    sudo chmod +s /usr/bin/screen
    sudo chmod 755 /var/run/screen
    cp $DIR/.screenrc ~
@@ -29,9 +36,9 @@ cp $DIR/.gitconfig ~
 cp $DIR/.bash_aliases ~
 cp $DIR/.npmrc ~
 cp $DIR/.vimrc ~
-cp $DIR/.vimrc $VIM/init.vim
-cp $DIR/.inputrc ~
 cp -r $DIR/.vim ~
+cp -r $DIR/.vim $NVIM
+cp $DIR/.inputrc ~
 cp $DIR/.mongorc.js ~
 
 if [ $TERM=='cigwin' ]; then
@@ -40,7 +47,7 @@ if [ $TERM=='cigwin' ]; then
     cp $DIR/ConEmu.xml ~/AppData/Roaming/
 fi
 
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+ln -s ~/.vimrc $NVIM/init.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein.sh
 sh ./installer.sh ~/.cache/dein
 
